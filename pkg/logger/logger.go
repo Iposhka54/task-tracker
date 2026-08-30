7be7399f5e71a67599354434b8b2b57c7d5253bb
Iposhka54 <yaroslavkusb@gmail.com>
2026-08-30 15:14:23 +0300

    feat: add logger pkg

---
+++ pkg/logger/logger.go
@@ -0,0 +1,45 @@
+package logger
+
+import (
+	"context"
+	"io"
+	"log/slog"
+	"os"
+
+	"go.opentelemetry.io/otel/trace"
+)
+
+type Config struct {
+	Level slog.Level `env:"LOG_LEVEL" env-default:"INFO"`
+}
+
+func New(cfg Config) *slog.Logger {
+	return NewWithWriter(os.Stdout, cfg)
+}
+
+func NewWithWriter(w io.Writer, cfg Config) *slog.Logger {
+	h := slog.NewJSONHandler(w, &slog.HandlerOptions{Level: cfg.Level})
+	return slog.New(&ctxHandler{Handler: h})
+}
+
+type ctxHandler struct {
+	slog.Handler
+}
+
+func (h *ctxHandler) Handle(ctx context.Context, r slog.Record) error {
+	if sc := trace.SpanContextFromContext(ctx); sc.IsValid() {
+		r.AddAttrs(
+			slog.String("request_id", sc.TraceID().String()),
+			slog.String("span_id", sc.SpanID().String()),
+		)
+	}
+	return h.Handler.Handle(ctx, r)
+}
+
+func (h *ctxHandler) WithAttrs(attrs []slog.Attr) slog.Handler {
+	return &ctxHandler{Handler: h.Handler.WithAttrs(attrs)}
+}
+
+func (h *ctxHandler) WithGroup(name string) slog.Handler {
+	return &ctxHandler{Handler: h.Handler.WithGroup(name)}
+}
